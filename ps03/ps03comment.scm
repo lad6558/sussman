026b37e132ad97d436ff97122d5edb2690ef8b6a
Andi Liu <andiliu@mit.edu>
2023-04-05 15:00:38 -0400

    Add ps03 essay and comments from GJS

---
+++ ps03/ps03comment.scm
@@ -0,0 +1,84 @@
+; Below is the reply from GJS
+
+;; Hi Andi,
+
+;;  I just read your ps03 essay.  It is very interesting.  I
+;; have carefully thought about it and I have learned something
+;; from it.  I am considering your points and want to argue
+;; about some of them.
+
+;; You are quite right that the purpose of this class is
+;; inspiration rather than mastery of technique, so I am
+;; certainly happy to give you ps03 credit for that work.
+
+;; Your first paragraph is a pretty good summary of the ideas I
+;; have been presenting in the class.
+
+;; Your second paragraph (beginning with "However") seems key
+;; to your argument.
+
+;; It is indeed true that the infrastructure required to
+;; support efficient generic procedures, for example, is quite
+;; complicated, but once implemented it is no longer necessary
+;; to look into it, in the same way you do not need to
+;; understand how a compiler works to use the source language.
+;; Sometimes it takes some serious effort to support a flexible
+;; superstructure.  The Jupyter notebook is exactly such a
+;; rather complicated infrastructure built to allow rapid
+;; prototyping.  I am showing you other infrastructures, to
+;; support flexibility, which are not covered by other
+;; paradigms.
+
+;; You argue that it is not necessarily a good idea to build a
+;; complex infrastructure to support higher-level flexibility
+;; because of there is not time, in the current business model,
+;; to do that.  You just have to produce novelty at a rapid
+;; rate to survive in that environment.  I think you are right,
+;; if you actually think that the current business model of
+;; development will continue into the future.  But I suspect
+;; that it will be gone shortly.  I suspect that in not very
+;; many years most "intellectual" jobs will be better done by
+;; AI systems and there will be no jobs needed (including
+;; professors) in that future world.  So the only value of
+;; creative work like programming will be for the pleasure of
+;; the work and sharing that work with friends (as art and
+;; music and literature are for the people who do it).  My kind
+;; of programming is partly aimed at that future.
+
+;; Your third and fourth paragraphs are really where we
+;; disagree.  I am quite aware of that other world, outside of
+;; my constructed palace.  I am trying to show you how to build
+;; a palace of your own, independent of the base system and
+;; language.  I know that is not the prevailing wisdom of the
+;; computer-science "professionals".  But I do not believe in
+;; the long-term viability of their approach, partly for the
+;; reason described above, but also because I see the very bad
+;; quality of the software that results from that approach.
+;; How much novelty do we want?  There are tradeoffs between
+;; novelty and longevity, quality, and reliability of systems
+;; based on software.
+
+;; But I want to emphasize that the SDF book is NOT about how
+;; how to adopt MY codebase and how to use it.  It is to
+;; illustrate the advantages of making such a "palace" and
+;; showing you that it is not very hard to make one for
+;; youself, on whatever platform you have, as needed.
+
+;; Anyway, I never worry about "a personal attack on my
+;; philosophy".  There are two reasons: Philosophy is meant to
+;; be attacked, because it is never "correct".  What is more
+;; important is that it is always OK to attack ideas, so long
+;; as we do not attack the people who have those ideas.  I
+;; learned very early that it is both fun and illuminating to
+;; argue about ideas.  So have fun :-)
+
+;; GJS
+
+
+;; BTW: I started programming in 1962, on an IBM 650 in machine
+;; language, so I have been programming for over 60 years
+;; (mostly recreationally)!  My friend Chris Hanson has been
+;; programming professionally for over 40 years (27 years for
+;; me at MIT and 16 years in industry, 10 of those years at
+;; Google), so together we have over 100 years of programming
+;; experience.
